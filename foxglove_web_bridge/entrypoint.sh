941a3e6af00df03c89941ea1d64e5c36d6aacbef
Philipp Schnattinger <Philipp.Schnattinger@node-robotics.com>
2025-10-11 22:27:22 +0200

    feat: add foxglove bridge

---
+++ foxglove_web_bridge/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+
+source "/root/nav2_ws/install/setup.bash" --
+exec "$@"
--- simulated_turtlebot/entrypoint.sh
+++ simulated_turtlebot/entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 set -e
 
-# setup gazebo and ros environment
-#source "/usr/share/gazebo/setup.sh" --
 source "/opt/ros/$ROS_DISTRO/setup.bash" --
 exec "$@"
